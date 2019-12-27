3a5b38f24b49d3a0916f104e02eff921702f046d
Oraida <oraidag@hotmail.com>
2019-12-27 00:47:47 -0600

    first commit

---
+++ src/app/shared/sitebar/sitebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sitebar',
+  templateUrl: './sitebar.component.html',
+  styles: []
+})
+export class SitebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
